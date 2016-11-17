1bddd9518eebad21c7ef6bf611d482cf6994ac2d
vgaidarji <veaceslav.gaidarji@yopeso.com>
2016-11-17 13:11:55 +0200

    Add jenkins scripts (install/update Android SDK)

---
+++ jenkins/update_android_sdk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PATH_TO_SDK=/Users/Shared/Jenkins/Home/tools/android-sdk
+
+/usr/bin/expect -c '
+set timeout -1   ;
+spawn '${PATH_TO_SDK}'/tools/android update sdk --no-ui --all;
+  expect {
+    "Do you accept the license" { exp_send "y\r" ; exp_continue }
+    eof
+}
+'
