2e3f240bb75b2ae9cc5a5152acc22f8e8746c512
vgaidarji <veaceslav.gaidarji@gmail.com>
2019-01-03 17:20:11 +0200

    Manually install APK for UI tests

--- .github/android-github-actions/ui-tests-on-emulator.sh
+++ .github/android-github-actions/ui-tests-on-emulator.sh
@@ -18,7 +18,11 @@ echo "Android Emulator started after $DURATION seconds."
 # prevents APK installation error
 sleep 60
 
-# run UI tests
-./gradlew connectedAndroidTest -PfabricApiKey=$FABRIC_API_KEY -PfabricApiSecret=$FABRIC_API_SECRET
+# manually install APK's and run UI tests
+./gradlew assembleDebug -PfabricApiKey=$FABRIC_API_KEY -PfabricApiSecret=$FABRIC_API_SECRET
+./gradlew assembleDebugAndroidTest -PfabricApiKey=$FABRIC_API_KEY -PfabricApiSecret=$FABRIC_API_SECRET
+$ANDROID_HOME/platform-tools/adb install app/build/outputs/apk/app-debug.apk
+$ANDROID_HOME/platform-tools/adb install app/build/outputs/apk/app-debug-androidTest.apk
+$ANDROID_HOME/platform-tools/adb shell am instrument -w -r -e debug false -e class com.vgaidarji.cimatters.LoginActivityTest com.vgaidarji.cimatters.test/android.support.test.runner.AndroidJUnitRunner
 
 kill-running-emulators
