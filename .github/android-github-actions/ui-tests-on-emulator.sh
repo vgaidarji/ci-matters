fdbec296ba341dcc2658d0e2ae223e3ce2e85b56
vgaidarji <veaceslav.gaidarji@gmail.com>
2019-01-04 13:40:19 +0200

    Externalize run-ui-tests script

--- .github/android-github-actions/ui-tests-on-emulator.sh
+++ .github/android-github-actions/ui-tests-on-emulator.sh
@@ -18,11 +18,6 @@ echo "Android Emulator started after $DURATION seconds."
 # prevents APK installation error
 sleep 60
 
-# manually install APK's and run UI tests
-./gradlew assembleDebug -PfabricApiKey=$FABRIC_API_KEY -PfabricApiSecret=$FABRIC_API_SECRET
-./gradlew assembleDebugAndroidTest -PfabricApiKey=$FABRIC_API_KEY -PfabricApiSecret=$FABRIC_API_SECRET
-$ANDROID_HOME/platform-tools/adb install app/build/outputs/apk/app-debug.apk
-$ANDROID_HOME/platform-tools/adb install app/build/outputs/apk/app-debug-androidTest.apk
-$ANDROID_HOME/platform-tools/adb shell am instrument -w -r -e debug false -e class com.vgaidarji.cimatters.LoginActivityTest com.vgaidarji.cimatters.test/android.support.test.runner.AndroidJUnitRunner
+run-ui-tests
 
 kill-running-emulators
