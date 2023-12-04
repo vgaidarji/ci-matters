b39a850edb9b8a11fcdddd1fe6da86a158994f12
vgaidarji <veaceslav.gaidarji@gmail.com>
2023-12-04 11:51:21 +0200

    Remove old Fabric.Crashlytics integration

--- app/src/main/java/com/vgaidarji/cimatters/CiMattersApplication.java
+++ app/src/main/java/com/vgaidarji/cimatters/CiMattersApplication.java
@@ -1,13 +1,11 @@
 package com.vgaidarji.cimatters;
 
 import android.app.Application;
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
 
 public class CiMattersApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Fabric.with(this, new Crashlytics());
+        // TODO: integrate with Crashlytics
     }
 }
